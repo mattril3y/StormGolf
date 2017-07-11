1f0ef005153f95747dd85fcf7fea3ba75e18f479
mattril3y <m@mjriley.net>
2017-07-11 21:31:36 +0100

    Initial commit

---
+++ StormGolf.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Web.Mvc;
+using StormGolf.Controllers;
+
+namespace StormGolf.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = (ViewResult)controller.Index();
+
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            var expectedVersion = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            var expectedRuntime = isMono ? "Mono" : ".NET";
+
+            // Assert
+            Assert.AreEqual(expectedVersion, result.ViewData["Version"]);
+            Assert.AreEqual(expectedRuntime, result.ViewData["Runtime"]);
+        }
+    }
+}
